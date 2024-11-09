50da6ad8f3dc64f5bdf41bd5b19aa3a7e9c9677c
6abrie1 <gabrieljv2019@gmail.com>
2024-11-09 12:54:52 -0300

    Implementação do envio de e-mail

---
+++ src/main/java/com/syntaxsquad/centro_treinamento/model/email/MailConfig.java
@@ -0,0 +1,28 @@
+package com.syntaxsquad.centro_treinamento.model.email;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("cen1ro.de.treinament0@gmail.com");
+        mailSender.setPassword("onns yboa qolp qeaa");
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
